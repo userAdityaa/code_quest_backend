2040d20157b9f96aeaff5ffb5a5e37c3a55a282b
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-03 04:01:34 +0530

    configure_handler: Configured the handler for better handling of login

--- handlers/github_auth.go
+++ handlers/github_auth.go
@@ -7,17 +7,22 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/userAdityaa/code_quest_backend/config"
 )
 
 func GithubLoginHandler(w http.ResponseWriter, r *http.Request) {
 	clientID := config.GetGithubClientID()
+	redirectURI := "http://localhost:8080/login/github/callback/"
+	encodedRedirectURI := url.QueryEscape(redirectURI)
+
 	redirectURL := fmt.Sprintf(
 		"https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s",
 		clientID,
-		"http://localhost:3000/login/github/callback",
+		encodedRedirectURI,
 	)
+
 	http.Redirect(w, r, redirectURL, http.StatusFound)
 }
 
@@ -28,8 +33,13 @@ func GithubCallBackHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	accessToken := getGithubAccessToken(code)
-	githubData := getGithubData(accessToken)
-	LoggedinHandler(w, r, githubData)
+	githubData, err := getGithubData(accessToken)
+	if err != nil {
+		http.Error(w, "Failed to get GitHub data", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	http.Redirect(w, r, fmt.Sprintf("http://localhost:3000/questions?username=%s&avatar_url=%s", githubData["login"], githubData["avatar_url"]), http.StatusFound)
 }
 
 func getGithubAccessToken(code string) string {
@@ -66,7 +76,7 @@ func getGithubAccessToken(code string) string {
 	return response.AccessToken
 }
 
-func getGithubData(accessToken string) string {
+func getGithubData(accessToken string) (map[string]interface{}, error) {
 	req, err := http.NewRequest("GET", "https://api.github.com/user", nil)
 	if err != nil {
 		log.Panic("Failed to create request")
@@ -79,6 +89,15 @@ func getGithubData(accessToken string) string {
 	}
 	defer resp.Body.Close()
 
-	data, _ := ioutil.ReadAll(resp.Body)
-	return string(data)
+	if resp.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(resp.Body)
+		return nil, fmt.Errorf("GitHub API error: %s", body)
+	}
+
+	var userData map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&userData); err != nil {
+		return nil, err
+	}
+
+	return userData, nil
 }
