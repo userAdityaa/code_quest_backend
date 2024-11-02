3ca9b6d9db6bb90d2ce99b0253b674b550703a4e
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-03 04:01:07 +0530

    modify_router: Modified the router for better routing of login

--- router/router.go
+++ router/router.go
@@ -2,13 +2,22 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/userAdityaa/code_quest_backend/handlers"
 )
 
 func InitializeRouter() *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 	r.Get("/", handlers.RootHandler)
 	r.Get("/login/github/", handlers.GithubLoginHandler)
-	r.Get("/login/github/callback", handlers.GithubCallBackHandler)
+	r.Get("/login/github/callback/", handlers.GithubCallBackHandler)
 	return r
 }
